39afc3d3714a750439832e627230cb5168e945fe
DanielBryjak <d.bryjak@op.pl>
2022-10-24 20:25:36 +0200

    zmiana world na ZORRO

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String name="Daniel";
+        String name="ZORRO";
         System.out.println("Hello "+name);
               }
     }
