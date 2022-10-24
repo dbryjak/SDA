6c06052ab0763016380162a79afff66308080ed2
DanielBryjak <d.bryjak@op.pl>
2022-10-24 20:23:16 +0200

    zmiana world na Daniel

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world");
-      }
+        String name="Daniel";
+        System.out.println("Hello "+name);
+              }
     }
